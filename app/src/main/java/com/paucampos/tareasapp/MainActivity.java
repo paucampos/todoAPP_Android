f27a37b5befa48e559239e6334f963589d7a5c78
Pau Campos <pau.campos89@gmail.com>
2026-06-02 13:02:45 -0400

    se elimana import sin uso

--- app/src/main/java/com/paucampos/tareasapp/MainActivity.java
+++ app/src/main/java/com/paucampos/tareasapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
